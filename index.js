e725be2f4f8b6a1e889cb1c1e844aae2a07b595a
wiyaou mangazie <mariejoseph144@gmail.com>
2020-10-23 06:05:56 -0400

    second commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var superheroes = require("supervillains");
+
+var mySuperheroName = superheroes.random();
+
+console.log(mySuperheroName);
+
+var supervillains = require('supervillains');
+ 
+supervillains.all;
+//=> ['Abattoir', 'Able Crown', …]
+ 
+var mySuperVillains = supervillains.random();
+
+console.log(mySuperVillains);
